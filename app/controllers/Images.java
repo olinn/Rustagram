d579494be527a9322b30a9d34acc39aec5ba5ea9
olinn <olinn85@gmail.com>
2013-11-21 13:24:24 +0000

    Imagecontroller

--- app/controllers/Images.java
+++ app/controllers/Images.java
@@ -4,7 +4,40 @@ import is.ru.honn.rustagram.domain.Image;
 import is.ru.honn.rustagram.domain.Comment;
 import is.ru.honn.rustagram.domain.User;
 
+import is.ru.honn.rustagram.domain.User;
+import is.ru.honn.rustagram.domain.UserRegistration;
+import is.ru.honn.rustagram.service.RustagramService;
+
+import is.ru.honn.rustagram.service.UserNotFoundException;
+import is.ru.honn.rustagram.service.UsernameExistsException;
+import play.cache.Cache;
+import play.data.*;
+import play.mvc.*;
+
+
+
+import views.html.index;
+import views.html.add_image;
+
+import static play.data.Form.form;
+
 public class Images extends AbstractRustagramController {
+
+    final static Form<Image> addImageForm = form(Image.class);
+
+    public static Result showAddImageForm()
+    {
+
+        return ok(add_image.render(addImageForm));
+
+    }
+
+    public static Result addImage()
+    {
+
+        return ok(index.render());
+
+    }
 
 
 }
